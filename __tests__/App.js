afe3d5a2fd9ac926b1311e467cd5ab91cb34c8be
อาสาฬ เครื่องสกุล <s6002041510080@Macmini213-21.local>
2019-03-14 16:42:04 +0700

    create App component

---
+++ __tests__/App.js
@@ -0,0 +1,17 @@
+// import library
+import React from 'react';
+
+// write component
+class App extends React.Component {
+    render() {
+        return (
+            //component about react native
+            <View>
+                <Text>Hello World</Text>
+            </View>
+
+        );
+    }
+}
+ // export
+ export default App; 
